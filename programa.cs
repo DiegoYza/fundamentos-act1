219563922faa596b261654dd7d20221cc299bc09
Diego Yzarra <yzarradiego@gmail.com>
2026-06-11 19:52:12 -0500

    Ahora si codigo completo

--- programa.cs
+++ programa.cs
@@ -1,13 +1,1 @@
-using System;
-
-class Program
-{
-    static void Main()
-    {
-        // Saludo simple requerido por la guía de laboratorio
-        Console.WriteLine("=====================================");
-        Console.WriteLine("¡Hola, Mundo! Saludo desde mi nueva rama.");
-        Console.WriteLine("Laboratorio de Git y GitHub completado.");
-        Console.WriteLine("=====================================");
-    }
-}
+// Programa de saludo
